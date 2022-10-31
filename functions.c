7ffad3ffa4425283eecca1206b64055f5d103fc1
rohxander <32710857+rohxander@users.noreply.github.com>
2022-11-01 01:32:31 +0530

    Update functions.c
    Added odd even function.

--- functions.c
+++ functions.c
@@ -269,3 +269,12 @@ int rev_num(int num){
       }
     return revNum;
 }
+
+void odd_even(int num){
+    // true if num is perfectly divisible by 2
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+}
+
